f301a46b941c9b3c6615c084353c12b73679ee99
Ashwani0044 <ashwanib707@gmail.com>
2026-08-27 14:02:16 +0530

    program 8 done

---
+++ lab3/prg8.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import { createReadStream } from 'fs';
+import { readFile } from 'fs/promises';
+
+const server = http.createServer(async (req, res) => {
+    if(req.url === "/stream") {
+        const stream = createReadStream("big.txt", { encoding : "utf-8"});
+        stream.pipe(res);
+    } else if(req.url === "/normal") {
+        const text = await readFile("big.txt");
+        res.end(text);
+    }
+});
+
+server.listen(3000, () => console.log("Server is running on http://localhost:3000"));
